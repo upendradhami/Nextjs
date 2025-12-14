97312ed362937c6e36f01635a18e28619d723a3b
upendradhami <upendradhami689@gmail.com>
2025-12-14 19:20:41 +0545

    notesareupdated

---
+++ 02-routings/app/dashboard/layout-all.tsx
@@ -0,0 +1,10 @@
+ function newpage() {
+  return (
+    <div> 
+
+        <h1 className="text-center font-bold text-2xl text-blue-500"> 
+           before welcoming to the layout new page , 
+            Welcome to Dashboard Page </h1>    
+    </div>
+  );
+}
---
+++ 02-routings/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+function sayingHello() {
+  return <h1 className="text-center font-bold text-2xl text-blue-500"> Welcome to Dashboard Page </h1>;
+} 
+
+export default function dashboard() {
+  return <>
+     <h1> this page is for dashboard </h1>
+  </>
+}
