51b7d40f4090deec2eda08e23ad8ac8307bdc343
upendradhami <upendradhami689@gmail.com>
2026-01-04 06:33:46 +0545

    initialized router handler demo

---
+++ 03-route_handler_dem0/app/comment/data.ts
@@ -0,0 +1,18 @@
+ export const comments: Array<object> = [
+  {id: 1,
+   comment: 'First comment ',
+  },
+  {
+    id: 1,
+    comment : 'Second comment',
+  },
+  {
+    id: 2,
+    comment: 'Third Comment',
+  },
+  {
+    id:3,
+    comment: '4th comment'
+  }
+
+]
---
+++ 03-route_handler_dem0/app/hello/route.ts
@@ -0,0 +1,4 @@
+  // export functions named after HTTP verbs (GET ,POST ,PATCH , PUT , DELETE)
+export async function GET() {
+  return new Response('Hello from route Handler learning section')
+}
